938687cab60071eae251ae8be9b0652e0c167bb8
FareedYarKhan1 <91247297+FareedYarKhan1@users.noreply.github.com>
2023-02-04 17:07:44 +0500

    Update settings.py

--- leech/settings.py
+++ leech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q7_sn^bmq$=4tzyedhcd!mrti=$5bc(ab+8$m&id$!@*5xrh@t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
